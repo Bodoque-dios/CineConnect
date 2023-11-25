2ce89e8b218c32412547760849fb253286b6d8e1
Matias Olea <mati.olea.thomsen@gmail.com>
2023-11-25 18:12:15 -0300

    functional demo chat

--- src/chat/Chat.jsx
+++ src/chat/Chat.jsx
@@ -5,43 +5,43 @@ import { Link } from "react-router-dom";
 const burro = {
   1: "¿Puedo decirte algo? ¡Qué increíble reacción ante tal situación! Fantástico.",
   2: {
-    2: "Claro, hablaba contigo. -",
-    3: "Oye, estuviste enorme. Esos cuates me querían como burro de carga, pero llegaste así ¡bam! patitas pa que las quiero. Se jueron de volada. Fue muy chistoso verlos correr. -",
+    2: "Claro, hablaba contigo.",
+    3: "Oye, estuviste enorme. Esos cuates me querían como burro de carga, pero llegaste así ¡bam! patitas pa que las quiero. Se jueron de volada. Fue muy chistoso verlos correr.",
   },
   3: {
-    4: "Que chido ser libre -",
-    5: "Es que… -",
-    6: "Yo no tengo amigos. -",
-    7: "Y no pienso ir al bosque yo solito. -",
-    8: "Hey tengo una ideota. -",
-    9: "Me quedaré contigo. -",
-    10: "Tu eres verdederamente una máquina de pelea. Haremos tronar a cualquiera. -",
+    4: "Que chido ser libre",
+    5: "Es que…",
+    6: "Yo no tengo amigos.",
+    7: "Y no pienso ir al bosque yo solito.",
+    8: "Hey tengo una ideota.",
+    9: "Me quedaré contigo.",
+    10: "Tu eres verdederamente una máquina de pelea. Haremos tronar a cualquiera.",
   },
   4: {
-    11: "Guau, eso sí que asusta. Y si el rugido no funciona, tu mal aliento seguro los desmaya. Necesitas unas pastillitas de menta, porque el hocico te apesta. -",
-    12: "Tan grueso el tufo como cuando me eché unas fresas podridas -",
-    13: "¡No, hombre! Pa inflar un zepelín con los gases que me salieron por detrás -",
+    11: "Guau, eso sí que asusta. Y si el rugido no funciona, tu mal aliento seguro los desmaya. Necesitas unas pastillitas de menta, porque el hocico te apesta.",
+    12: "Tan grueso el tufo como cuando me eché unas fresas podridas.",
+    13: "¡No, hombre! Pa inflar un zepelín con los gases que me salieron por detrás.",
   },
   5: {
-    14: "Te diré por qué. -",
-    15: "Porque estoy solito, no hay nadie aquí a mi lado. No habrá problemas hoy. De mí ya se han burlado. Amigos debes tener. -",
+    14: "Te diré por qué.",
+    15: "Porque estoy solito, no hay nadie aquí a mi lado. No habrá problemas hoy. De mí ya se han burlado. Amigos debes tener.",
   },
-  6: "¡Wow! Solo un amigo dice la verdad tan cruda -",
-  7: "¿Eres…...muy alto? -",
-  8: "Nones -",
+  6: "¡Wow! Solo un amigo dice la verdad tan cruda",
+  7: "¿Eres…...muy alto?",
+  8: "Nones",
   9: {
     19: "¿De veritas, de veritas? -",
-    20: "Hasta me caes bien. ¿Cómo te llamas? -",
+    20: "Hasta me caes bien. ¿Cómo te llamas?",
   },
   10: {
-    21: "¡Shrek! -",
-    22: "¿Sabes que me gusta de ti, Shrek? Que eres de los cuates a los que les vale un comino lo que los demás piensen de ti. Eso es chido. Mis respetos. Eres una buena bestia. -",
-    23: "Blee mira eso. Quien querría vivir en ese cuchitril. -",
+    21: "¡Shrek!",
+    22: "¿Sabes que me gusta de ti, Shrek? Que eres de los cuates a los que les vale un comino lo que los demás piensen de ti. Eso es chido. Mis respetos. Eres una buena bestia.",
+    23: "Blee mira eso. Quien querría vivir en ese cuchitril.",
   },
   11: {
-    24: "¡Oh, es adorable y hermoso! Eres un gran decorador…lo que alguien con talento puede hacer con poco presupuesto. -",
-    25: "¡Oh, qué linda piedra! Un detalle soberbio. -",
-    26: "¿Cuidado con el ogro? ¿No creo que tengas muchos amigos o sí? -",
+    24: "¡Oh, es adorable y hermoso! Eres un gran decorador…lo que alguien con talento puede hacer con poco presupuesto.",
+    25: "¡Oh, qué linda piedra! Un detalle soberbio.",
+    26: "¿Cuidado con el ogro? ¿No creo que tengas muchos amigos o sí?",
   },
   12: {
     27: "Ah, a mí también, es otra cosa que tenemos en común. Odio cuando alguien está dando lata. Le echas las indirectas y como no entiende pues no se va. Y luego el silencio a poco no",
@@ -52,34 +52,47 @@ const burro = {
 const Chat = () => {
   // useParams hook from React Router is used to access the parameters in the URL
   const { id } = useParams();
-  const [messages, setMessages] = useState([
-    { mensaje: "Hola", wasSent: true },
-    { mensaje: "Hola", wasSent: false },
-  ]);
-  const [messageIndex, setMessageIndex] = useState(1);
-  const addmesage = (message, wasSent = true) => {
-    console.log(messages);
-    setMessages([...messages, { mensaje: message, wasSent: wasSent }]);
-  };
-  const response = () => {
-    const mymessages = burro[messageIndex];
-    if (mymessages instanceof String) {
-      addmesage(mymessages);
-    } else if (mymessages instanceof Object) {
-      const keys = Object.keys(mymessages);
-      let delay = 0;
-      for (let i = 0; i < keys.length; i++) {
-        addmesage(mymessages[i], false);
-      }
-    }
-    setMessageIndex(messageIndex + 1);
-    console.log(mymessages);
+  const [messages, setMessages] = useState([{ mensaje: burro[1], wasSent: false }]);
+  const [messageIndex, setMessageIndex] = useState(2);
+
+
+  const scrollToBottom = () => {
+    const element = document.getElementById("scrollToThis");
+    element.scrollIntoView({ behavior: "smooth" });
   };
 
-  useEffect(() => {
+
+  const addmesage = (message, wasSent = true) => {
+
+    let messageArray = [{mensaje: message, wasSent: true}]
     
-    setInterval(() => {response();}, 1000);
-  }, []);
+    const response = burro[messageIndex];
+
+    if ( typeof response === 'string') {
+
+      messageArray.push({ mensaje: response, wasSent: false });
+      console.log(messageArray);
+
+    } else if (response instanceof Object) {
+
+      const keys = Object.keys(response);
+      for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+        const value = response[key];
+        messageArray.push({ mensaje: value, wasSent: false });
+      }
+    }
+
+    console.log(messageArray);
+    setMessages([...messages, ...messageArray]);
+    messageIndex === 12 ? setMessageIndex(2) : setMessageIndex(messageIndex + 1);
+  };
+
+
+
+  useEffect(() => {
+    scrollToBottom();
+  }, );
 
   return (
     <div className="relative h-screen max-h-screen w-full overflow-hidden bg-gradient-to-b from-primary-950 via-primary-900  to-primary-950 font-libre">
@@ -114,7 +127,7 @@ const Chat = () => {
 
       <ChatBox messages={messages} />
 
-      <ChatInput addmessage={addmesage}  response={response}/>
+      <ChatInput addmessage={addmesage}/>
     </div>
   );
 };
@@ -122,7 +135,7 @@ const Chat = () => {
 function Mensaje({ wasSent, mensaje }) {
   if (!wasSent) {
     return (
-      <div className="ml-4 mt-2 h-fit  w-fit rounded bg-azul p-2 py-2 text-white">
+      <div className="ml-4 max-w-[70%] mt-2 h-fit  w-fit rounded bg-azul p-2 py-2 text-white">
         {mensaje}
       </div>
     );
@@ -130,6 +143,7 @@ function Mensaje({ wasSent, mensaje }) {
     return (
       <div className="mr-4 mt-2 h-fit w-fit self-end rounded bg-naranjo p-2 py-2">
         {mensaje}
+        
       </div>
     );
   }
@@ -137,15 +151,16 @@ function Mensaje({ wasSent, mensaje }) {
 
 function ChatBox({ messages }) {
   return (
-    <div className="flex min-h-[75vh] w-screen flex-col overflow-hidden overflow-y-auto text-lg ">
+    <div className="flex h-[75vh]  w-screen flex-col overflow-auto overflow-y-scroll text-lg ">
       {messages.map((message, key) => {
         return <Mensaje key={key} {...message} />;
       })}
+      <div id="scrollToThis"></div>
     </div>
   );
 }
 
-function ChatInput({ addmessage, response }) {
+function ChatInput({ addmessage }) {
   const handleSubmmit = (e) => {
     e.preventDefault();
     const message = e.target[0].value;
@@ -154,7 +169,6 @@ function ChatInput({ addmessage, response }) {
       addmessage(message);
       e.target[0].value = "";
     }
-    response()
     
   };
 
